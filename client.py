69a99870d28f92847e855421ddd4a831db399271
Nosy <mamukov2000@mail.ru>
2019-12-24 18:15:33 +0300

    блин так кушать хочется

--- client.py
+++ client.py
@@ -1,14 +1,40 @@
+#
+#
 import socket
 import pickle
+import random
 
-HOST = '127.0.0.1'
+def encrypt(m, k):
+    return ''.join([chr((ord(x) + k) % 65536) for x in m])
+
+def decrypt(m, k):
+    return ''.join([chr((ord(x) - k) % 65536) for x in m])
+
+HOST = 'localhost'
 PORT = 8080
 
 sock = socket.socket()
 sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
+p, g, a = random.randint(1, 1000), random.randint(1, 1000), random.randint(1, 1000)
+
 A = g ** a % p
+
 sock.send(pickle.dumps((p, g, A)))
+
+
+
+while True:
+    tt = input('Сообщение: ')
+    if tt == "":
+        break
+    
+    sock.send(pickle.dumps(tt))
+    msg = sock.recv(1024)
+    B = pickle.loads(msg)[0]
+    msg = pickle.loads(msg)[1]
+    print(msg)
+    k = B ** a % p
+    print(decrypt(msg, k))
 
 sock.close()
--- server.py
+++ server.py
@@ -1,7 +1,16 @@
+#
+#
 import socket
 import pickle
+import random
 
-HOST = '127.0.0.1'
+def encrypt(m, k):
+    return ''.join([chr((ord(x) + k) % 65536) for x in m])
+
+def decrypt(m, k):
+    return ''.join([chr((ord(x) - k) % 65536) for x in m])
+
+HOST = 'localhost'
 PORT = 8080
 
 sock = socket.socket()
@@ -9,7 +18,20 @@ sock.bind((HOST, PORT))
 sock.listen(1)
 conn, addr = sock.accept()
 
+b = random.randint(1, 1000)
 msg = conn.recv(1024)
-print(pickle.loads(msg))
+(p, g, A) = pickle.loads(msg)
+
+while True:
+    msg = conn.recv(1024)
+    if not msg:
+        break
+    
+    msg = pickle.loads(msg)
+    b = random.randint(1, 1000)
+    B = g ** b % p
+    k = A ** b % p
+    print(k)
+    conn.send(pickle.dumps((B, encrypt(msg, k))))
 
 conn.close()
